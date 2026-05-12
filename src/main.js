5936069f9bcd3608d7f3f00f4845a275fc9847ba
Rhett Bauchop <rhett.bauchop@gmail.com>
2026-05-12 17:04:31 +0800

    Started on the Signup and login pages

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+// import { PrLock } from 'oh-vue-icons/icons'
+
+import * as PrIcons from 'oh-vue-icons/icons/pr'
+const Pr = Object.values({ ...PrIcons })
+
+addIcons(...Pr)
+
 const app = createApp(App)
 
 app.use(router)
-
+app.component('v-icon', OhVueIcon)
 app.mount('#app')
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,29 +1,48 @@
 <template>
-  <div class="auth-page">
-    <div class="auth-card">
-      <h2>Welcome Back</h2>
+  <main class="auth-page">
+    <section class="header">
+      <img class="logo" src="../assets/logos/clubsAtlasLogo.svg" alt="ClubsAtlas Logo" />
+      <h1>ClubsAtlas</h1>
+    </section>
+    <div class="hero">
+      <div class="formTitle">
+        <h2>Welcome Back</h2>
+        <p>Log in to see your clubs and upcoming events</p>
+      </div>
 
       <div class="field">
+        <v-icon name="PrLock" />
         <label>Email</label>
-        <input v-model="email" type="email" placeholder="you@example.com" />
+        <input v-model="email" type="email" placeholder="Email Address" />
       </div>
 
       <div class="field">
         <label>Password</label>
-        <input v-model="password" type="password" placeholder="••••••••" />
+        <input v-model="password" type="password" placeholder="Password" />
       </div>
 
-      <button @click="handleLogin" class="btn btn-primary">Log In</button>
+      <BaseButton @click="handleLogin">Log In</BaseButton>
 
-      <p>
-        Don't have an account?
-        <RouterLink to="/signup">Sign up</RouterLink>
-      </p>
+      <div class="formTitle">
+        <p>
+          Don't have an account?
+          <RouterLink class="textLink" to="/signup">Sign up here!</RouterLink>
+        </p>
+      </div>
     </div>
-  </div>
+    <section class="footer">
+      <p>Terms - Privacy - Contact</p>
+      <!-- <RouterLink to="/login">Terms</RouterLink>
+      <p>-</p>
+      <RouterLink to="/login">Privacy</RouterLink>
+      <p>-</p>
+      <RouterLink to="/login">Contact</RouterLink> -->
+    </section>
+  </main>
 </template>
 
 <script setup>
+import BaseButton from '@/components/BaseButton.vue'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -37,3 +56,24 @@ function handleLogin() {
   router.push('/') // Programmatic navigation after login
 }
 </script>
+
+<style scoped>
+.hero {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  flex: 1;
+}
+
+.formTitle {
+  text-align: left;
+  color: var(--color-text);
+}
+
+.textLink {
+  color: var(--color-brandText);
+}
+</style>
